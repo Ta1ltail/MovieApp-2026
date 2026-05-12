8455adb0a205c538b49c2aa552621045c3c40318
Justin Javier <jjavier292002@gmail.com>
2026-05-12 22:51:43 +0800

    test

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+const Card = ({ title }) => {
+
+  const [hasLiked, setHasLiked] = useState(false);
+  return ( 
+    <div className='card'>
+      <h2>{title}</h2>
+
+      <button className='button' onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? '❤️' : '🤍'}</button>
+    </div>)
+}
+
+const App = () => {
+  return (
+    <div className="card-container">
+      <Card title ="Inception"/>
+      <Card title ="Titanic"/>
+      <Card title ="Avatar"/>
+    </div>
+  )
+}
+
+export default App
